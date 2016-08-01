e2ac94c13043f1dda30de48d9766ef7d5721f1e9
Manal Afquir <manal.afquir@gmail.com>
2016-08-01 18:02:27 +0200

    #10 organisation des fichiers

--- source/app/boot.ts
+++ source/app/boot.ts
@@ -1,9 +1,8 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {FormCondDistri} from './form_cond_distri';
-import {Formulaire} from './formulaire';
+import {FormCondDistri} from './conduite_distri/form_cond_distri';
+import {Formulaire} from './common/formulaire';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {enableProdMode} from '@angular/core';
 
 enableProdMode();
 bootstrap(FormCondDistri,[Formulaire,HTTP_PROVIDERS]);
-//bootstrap(Formulaire,[FormCondDistri,HTTP_PROVIDERS]);
