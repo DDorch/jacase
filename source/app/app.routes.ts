bc6cbf4545fc454c8100d721a4b4ea1bca353668
root <manal.afquir@gmail.com>
2016-08-09 17:19:21 +0200

    #10 ajout du regime_uniforme.config.json

--- source/app/app.routes.ts
+++ source/app/app.routes.ts
@@ -4,7 +4,11 @@ import {FormLechaptCalmon} from './lechapt_calmon/lechapt_calmon';
 
 const routes: RouterConfig = [
     {path:'cond_distri', component: FormCondDistri} ,
-    {path:'lechapt_calmon', component: FormLechaptCalmon }
+    {path:'lechapt_calmon', component: FormLechaptCalmon } ,
+   /* {path:'ouvrages', component: FormOuvrages },
+    {path:'lechapt_calmon', component: FormRemous },
+    {path:'lechapt_calmon', component: FormRegUniforme },
+    {path:'lechapt_calmon', component: FormSectionParam }*/
 ];
 
 export const appRouterProviders = [ provideRouter(routes) ];
